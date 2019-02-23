2f97538f9893af482d6c132a3d8f86fb4601ff75
radek-git <radoslaw.szpionkowski@gmail.com>
2019-02-23 17:42:00 +0100

    update

--- src/main/java/com/radek/rentals/service/UserService.java
+++ src/main/java/com/radek/rentals/service/UserService.java
@@ -7,7 +7,6 @@ import com.radek.rentals.entity.User;
 import com.radek.rentals.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
