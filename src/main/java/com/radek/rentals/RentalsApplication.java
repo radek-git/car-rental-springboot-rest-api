5d7b9c6ea0812bdfbc518493cf2aecbfb88bbc43
radek-git <radoslaw.szpionkowski@gmail.com>
2019-02-26 00:03:04 +0100

    upload/download files

--- src/main/java/com/radek/rentals/RentalsApplication.java
+++ src/main/java/com/radek/rentals/RentalsApplication.java
@@ -2,8 +2,10 @@ package com.radek.rentals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileStorageProperties.class)
 public class RentalsApplication {
 
     public static void main(String[] args) {
